1674875f06518f4bfe189da4c41987bb64e40bb7
Jeonghoon Oh <ihoon9203@gmail.com>
2023-07-11 13:59:29 +0900

    Initial Commit

---
+++ BackgroundCounter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BackgroundCounter
+//
+//  Created by Jeonghoon Oh on 2023/07/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
