958dccb8d4fb4029cb2592c4904fc9051bae292e
Jeonghoon Oh <ihoon9203@gmail.com>
2023-07-14 15:51:38 +0900

    implemented basics

--- BackgroundCounter/AppDelegate.swift
+++ BackgroundCounter/AppDelegate.swift
@@ -6,14 +6,26 @@
 //
 
 import UIKit
+import BackgroundTasks
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        UserDefaults.standard.set(0, forKey: "count")
         // Override point for customization after application launch.
+        // 1. App Refresh Task
+        BGTaskScheduler.shared.register(forTaskWithIdentifier: "com.runnan.background_fetch", using: nil) { task in
+            self.handleAppRefresh(task: task as! BGAppRefreshTask)
+        }
+        BGTaskScheduler.shared.register(forTaskWithIdentifier: "com.runnan.background_fetch_long", using: nil) { task in
+            self.handleLongAppRefresh(task: task as! BGAppRefreshTask)
+        }
+        
+        // 2. Processing Task
+//        BGTaskScheduler.shared.register(forTaskWithIdentifier: "com.nearbrain.CLTester.refresh_process", using: nil) { task in
+//            self.handleProcessingTask(task: task as! BGProcessingTask) // 타입 캐스팅 유의 (BG'Processing'Task)
+//        }
         return true
     }
 
@@ -30,7 +42,52 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
+    func handleAppRefresh(task: BGAppRefreshTask) {
+        scheduleAppRefresh()
+        let value = UserDefaults.standard.integer(forKey: "count")
+        UserDefaults.standard.set(value + 1, forKey: "count")
+        task.expirationHandler = {
+            task.setTaskCompleted(success: false)
+            NotificationCenter.default.post(name: NSNotification.Name(rawValue: "two"), object: nil)
+        }
+        task.setTaskCompleted(success: false)
+        NotificationCenter.default.post(name: NSNotification.Name(rawValue: "three"), object: nil)
+    }
+    func handleLongAppRefresh(task: BGAppRefreshTask) {
+        UserDefaults.standard.set(20000, forKey: "count")
+        task.expirationHandler = {
+            task.setTaskCompleted(success: false)
+            NotificationCenter.default.post(name: NSNotification.Name(rawValue: "two"), object: nil)
+        }
+        task.setTaskCompleted(success: false)
+        NotificationCenter.default.post(name: NSNotification.Name(rawValue: "three"), object: nil)
+    }
+    func scheduleAppRefresh() {
+        let request = BGAppRefreshTaskRequest(identifier: "com.runnan.background_fetch")
 
+        request.earliestBeginDate = Date(timeIntervalSinceNow: 5 * 60)
+        do {
+            try BGTaskScheduler.shared.submit(request)
+            NotificationCenter.default.post(name: NSNotification.Name(rawValue: "one"), object: nil)
+            // Set a breakpoint in the code that executes after a successful call to submit(_:).
+        } catch {
+            NotificationCenter.default.post(name: NSNotification.Name(rawValue: "fail"), object: nil)
+            print("\(Date()): Could not schedule app refresh: \(error)")
+        }
+    }
+    func scheduleLongAppRefresh() {
+        let request = BGAppRefreshTaskRequest(identifier: "com.runnan.background_fetch_long")
+
+        request.earliestBeginDate = Date(timeIntervalSinceNow: 12 * 60 * 60)
+        do {
+            try BGTaskScheduler.shared.submit(request)
+            NotificationCenter.default.post(name: NSNotification.Name(rawValue: "one"), object: nil)
+            // Set a breakpoint in the code that executes after a successful call to submit(_:).
+        } catch {
+            NotificationCenter.default.post(name: NSNotification.Name(rawValue: "fail"), object: nil)
+            print("\(Date()): Could not schedule app refresh: \(error)")
+        }
+    }
 
 }
 
--- BackgroundCounter/ViewController.swift
+++ BackgroundCounter/ViewController.swift
@@ -9,11 +9,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var status: UILabel!
+    @IBOutlet weak var count: UILabel!
     override func viewDidLoad() {
+        NotificationCenter.default.addObserver(self, selector: #selector(didSentBG), name: NSNotification.Name("one"), object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(didFailBG), name: NSNotification.Name("two"), object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(didFinishBG), name: NSNotification.Name("three"), object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(didFailSending), name: NSNotification.Name("fail"), object: nil)
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func setLongBGTask(_ sender: Any) {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        appDelegate.scheduleLongAppRefresh()
+    }
+    @IBAction func submitBGTask(_ sender: Any) {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        appDelegate.scheduleAppRefresh()
+    }
+    @IBAction func updateView(_ sender: Any) {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        count.text = String(UserDefaults.standard.integer(forKey: "count"))
+        status.text = "updated"
+    }
+    @objc func didSentBG() {
+        status.text = "sent"
+    }
+    @objc func didFailBG() {
+        status.text = "fail"
+    }
+    @objc func didFinishBG() {
+        count.text = String(UserDefaults.standard.integer(forKey: "count"))
+        status.text = "finish"
+    }
+    @objc func didFailSending() {
+        count.text = String(UserDefaults.standard.integer(forKey: "count"))
+        status.text = "ff"
+    }
 }
-
